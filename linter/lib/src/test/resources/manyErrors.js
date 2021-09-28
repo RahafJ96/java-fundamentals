c55552f8c86786a7ce0c6f06a4aba69350f1d135
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-09-28 20:51:37 +0300

    linter

---
+++ linter/lib/src/test/resources/manyErrors.js
@@ -0,0 +1,140 @@
+function postBook(req, res) {
+  const { bookName, describtion, image, email } = req.body
+
+  User.find({ email: email }, (err, data) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    } else {
+      data[0].books.push({bookName,describtion,image,})
+      data[0].save()
+      res.status(201).send(data[0].books)
+    }
+  });
+}
+function postBook(req, res) {
+  const { bookName, describtion, image, email } = req.body
+
+  User.find({ email: email }, (err, data) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    } else {
+      data[0].books.push({bookName,describtion,image,})
+      data[0].save()
+      res.status(201).send(data[0].books)
+    }
+  });
+}
+function postBook(req, res) {
+  const { bookName, describtion, image, email } = req.body
+
+  User.find({ email: email }, (err, data) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    } else {
+      data[0].books.push({bookName,describtion,image,})
+      data[0].save()
+      res.status(201).send(data[0].books)
+    }
+  });
+}
+function postBook(req, res) {
+  const { bookName, describtion, image, email } = req.body
+
+  User.find({ email: email }, (err, data) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    } else {
+      data[0].books.push({bookName,describtion,image,})
+      data[0].save()
+      res.status(201).send(data[0].books)
+    }
+  });
+}
+function postBook(req, res) {
+  const { bookName, describtion, image, email } = req.body
+
+  User.find({ email: email }, (err, data) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    } else {
+      data[0].books.push({bookName,describtion,image,})
+      data[0].save()
+      res.status(201).send(data[0].books)
+    }
+  });
+}
+function postBook(req, res) {
+  const { bookName, describtion, image, email } = req.body
+
+  User.find({ email: email }, (err, data) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    } else {
+      data[0].books.push({bookName,describtion,image,})
+      data[0].save()
+      res.status(201).send(data[0].books)
+    }
+  });
+}
+function postBook(req, res) {
+  const { bookName, describtion, image, email } = req.body
+
+  User.find({ email: email }, (err, data) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    } else {
+      data[0].books.push({bookName,describtion,image,})
+      data[0].save()
+      res.status(201).send(data[0].books)
+    }
+  });
+}
+function postBook(req, res) {
+  const { bookName, describtion, image, email } = req.body
+
+  User.find({ email: email }, (err, data) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    } else {
+      data[0].books.push({bookName,describtion,image,})
+      data[0].save()
+      res.status(201).send(data[0].books)
+    }
+  });
+}
+function postBook(req, res) {
+  const { bookName, describtion, image, email } = req.body
+
+  User.find({ email: email }, (err, data) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    } else {
+      data[0].books.push({bookName,describtion,image,})
+      data[0].save()
+      res.status(201).send(data[0].books)
+    }
+  });
+}
+function postBook(req, res) {
+  const { bookName, describtion, image, email } = req.body
+
+  User.find({ email: email }, (err, data) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    } else {
+      data[0].books.push({bookName,describtion,image,})
+      data[0].save()
+      res.status(201).send(data[0].books)
+    }
+  });
+}
